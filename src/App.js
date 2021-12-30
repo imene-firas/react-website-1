8733c5dd00f94f815cfe44eee47262159f0189d7
imene-firas <fi.imenefiras@gmail.com>
2021-12-30 11:36:49 +0100

    Revert "Revert "create first components""
    This reverts commit 35733f6a233eca14060b1337c53b619ff8eb41c9.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import Test from "./components/Pages/Test";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <Navbar>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </Navbar>
+      </Router>
+    </>
   );
 }
 
